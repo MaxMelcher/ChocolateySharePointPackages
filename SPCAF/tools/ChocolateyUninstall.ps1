423253357c24697a286b776300afc93723a9f752
MaxMelcher <melcher.maximilian@gmail.com>
2013-10-23 19:12:59 +0200

    GUID updated  - uninstaller does not work for me.

--- SPCAF/tools/ChocolateyUninstall.ps1
+++ SPCAF/tools/ChocolateyUninstall.ps1
@@ -2,7 +2,7 @@ try
 { 
 	# could be found at
 	# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{44478D64-5F45-45AD-B90C-CF47E7144C79}
-	Uninstall-ChocolateyPackage 'SPCAF' 'msi' '{44478D64-5F45-45AD-B90C-CF47E7144C79} /norestart /qn' 	  
+	Uninstall-ChocolateyPackage 'SPCAF' 'msi' '{1FAE356A-7D5A-B344-D95C-02F975D80AFC} /norestart /qn' 	  
 	
 	Write-ChocolateySuccess "SPCAF"
 }
