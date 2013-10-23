c00b317ba923f14f9ab3ee959d17d6506c128467
MaxMelcher <melcher.maximilian@gmail.com>
2013-10-23 18:53:41 +0200

    SPCAF 4.1.1 - uninstaller broken.

--- SPCAF/tools/ChocolateyInstall.ps1
+++ SPCAF/tools/ChocolateyInstall.ps1
@@ -1,15 +1,15 @@
 try 
 { 
-	$targetUrl = "http://visualstudiogallery.msdn.microsoft.com/d3b2aaf7-0d6a-4995-a4e5-4153c57e3889/file/100303/5/SPCAF.Setup.4.0.6.1005.msi"
+	$targetUrl = "http://visualstudiogallery.msdn.microsoft.com/d3b2aaf7-0d6a-4995-a4e5-4153c57e3889/file/100303/13/SPCAF.Setup.4.1.1.1206.msi"
 	$targetFileName = [System.IO.Path]::GetFileName($targetUrl)
 
 	Write "Loading and installing file:[$targetFileName] from URL:[$targetUrl]"
 	Install-ChocolateyPackage $targetUr 'msi' '/quiet' $targetUrl
 	  
-	Write-ChocolateySuccess 'SPCAFOnline'
+	Write-ChocolateySuccess 'SPCAF'
 }
 catch 
 {
-  	Write-ChocolateyFailure 'SPCAFOnline' "$($_.Exception.Message)"
+  	Write-ChocolateyFailure 'SPCAF' "$($_.Exception.Message)"
   	throw 
 }
--- SPCAF/tools/ChocolateyUninstall.ps1
+++ SPCAF/tools/ChocolateyUninstall.ps1
@@ -4,10 +4,10 @@ try
 	# HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{44478D64-5F45-45AD-B90C-CF47E7144C79}
 	Uninstall-ChocolateyPackage 'SPCAF' 'msi' '{44478D64-5F45-45AD-B90C-CF47E7144C79} /norestart /qn' 	  
 	
-	Write-ChocolateySuccess "SPCAFOnline"
+	Write-ChocolateySuccess "SPCAF"
 }
 catch 
 {
-  	Write-ChocolateyFailure 'SPCAFOnline' "$($_.Exception.Message)"
+  	Write-ChocolateyFailure 'SPCAF' "$($_.Exception.Message)"
   	throw 
 }
